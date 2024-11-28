26ebbc60464bcc119849ae3c47167e9350b3ec32
Galinsukiy <uchastnic2@mail.ru>
2024-11-28 17:11:15 +0300

    Проект 2 участника готов

--- TCP Chat/Program.cs
+++ TCP Chat/Program.cs
@@ -10,6 +10,22 @@ namespace TCP_Chat
     {
         static void Main(string[] args)
         {
+	    TcpClient client = new TcpClient();
+            client.Connect(Console.ReadLine(), Int32.Parse(Console.ReadLine()));
+            Task.Run(() =>
+            {
+                StreamReader sr = new StreamReader(client.GetStream());
+                while (true)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            });
+            StreamWriter sw = new StreamWriter(client.GetStream());
+            while (true)
+            {
+                sw.WriteLine(Console.ReadLine());
+                sw.Flush();
+            }
         }
     }
 }
