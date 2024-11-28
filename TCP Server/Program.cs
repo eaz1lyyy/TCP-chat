479e33a847f30ecd96ddbd56956585eda56072a6
tes1 <h.anonimus@mail.ru>
2024-11-28 17:33:33 +0300

    Участнику 1

--- TCP Server/Program.cs
+++ TCP Server/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TCP_Server
 {
     internal class Program
     {
+        private static List<StreamWriter> clients = new List<StreamWriter>();
+        private static TcpListener server;
+
         static void Main(string[] args)
         {
+            server = new TcpListener(IPAddress.Any, 25565);
+            server.Start();
+            Console.WriteLine("Сервер запущен...");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                StreamReader sr = new StreamReader(client.GetStream());
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                clients.Add(sw);
+                Console.WriteLine("Клиент подключен.");
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        string msg = sr.ReadLine();
+                        foreach (StreamWriter writer in clients)
+                        {
+                            if (writer != sw)
+                            {
+                                writer.WriteLine(msg);
+                                writer.Flush();
+                            }
+                        }
+                    }
+                });
+            }
         }
     }
 }
